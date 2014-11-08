a7fd1b2e43455c54d928ad5b046911f5c3b7d010
Victor Jiao <VjiaoBlack@gmail.com>
2014-11-07 19:42:12 -0600

    fix design

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.onload = function() {
     opacity = 0;
     var fadeToDoc = setInterval(function() {
 
-        opacity += 0.04;
+        opacity += 0.06;
         if (opacity > 1) {
         document.getElementsByTagName("body")[0].style.opacity = opacity;
             opacity = 1;
