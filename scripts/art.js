32c87809e289826b59495da30b8e4e232abcb7e5
Victor Jiao <VjiaoBlack@gmail.com>
2019-01-06 23:54:46 -0800

    upload new art; update some writing

--- scripts/art.js
+++ scripts/art.js
@@ -8,11 +8,23 @@ $(function() {
     });
  
     //----- CLOSE
-    $('[data-popup-close]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-close');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
- 
- 		console.log("close")
-        e.preventDefault();
-    });
+    $(document).on('click', function(e) {
+        var all_popups = $(document).find('.popup');
+        var i;
+        for (i = 0; i < all_popups.length; i++) {
+            // display: only look at popup displays that are on
+            // opacity: don't close transitioning popups
+            if ($(all_popups[i]).prop("style")['display'] == 'block' && 
+                $(all_popups[i]).prop("style")['opacity'] == "") {
+                var close_popup = $(all_popups[i]).find("[data-popup-close]");
+                var targeted_popup_class = $(close_popup).attr('data-popup-close');
+
+                $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+                console.log("close");
+                e.preventDefault();
+
+                console.log();
+            }
+        }
+    })
 });
